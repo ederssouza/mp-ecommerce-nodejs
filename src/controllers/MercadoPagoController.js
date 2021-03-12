f5a516dd80e46011425cc0efb4cbe1364433dfac
Eder Sampaio <edersampaio@outlook.com.br>
2021-03-12 15:57:39 -0300

    Remove old methods

--- src/controllers/MercadoPagoController.js
+++ src/controllers/MercadoPagoController.js
@@ -1,39 +1,12 @@
 'use strict'
 
 class MercadoPagoController {
-  index (req, res) {
-    return res.json({
-      path: 'index',
-      status: 'ok'
-    })
-  }
-
-  show (req, res) {
-    return res.json({
-      path: 'show',
-      status: 'ok'
-    })
-  }
-
   store (req, res) {
-    return res.json({
-      path: 'store',
-      status: 'ok'
-    })
+    return res.json({})
   }
 
   update (req, res) {
-    return res.json({
-      path: 'update',
-      status: 'ok'
-    })
-  }
-
-  delete (req, res) {
-    return res.json({
-      path: 'delete',
-      status: 'ok'
-    })
+    return res.json({})
   }
 }
 
