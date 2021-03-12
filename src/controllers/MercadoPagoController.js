de1ddc22996b91321147f955b46180f79d3a7d2a
Eder Sampaio <edersampaio@outlook.com.br>
2021-03-12 16:55:40 -0300

    Capture request body

--- src/controllers/MercadoPagoController.js
+++ src/controllers/MercadoPagoController.js
@@ -2,7 +2,16 @@
 
 class MercadoPagoController {
   store (req, res) {
-    return res.json({})
+    try {
+      const { id, img, title, price, unit } = req.body
+      return res.json({ id, img, title, price, unit })
+    } catch (error) {
+      const { status: code, message } = error
+      return res.status(status).json({
+        code,
+        message
+      })
+    }
   }
 
   update (req, res) {
