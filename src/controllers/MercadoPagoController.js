17e3438f6844dd8c62acd05a9da0dfc65d62be93
Eder Sampaio <edersampaio@outlook.com.br>
2021-03-12 18:33:30 -0300

    Add mercadoPago helper module

--- src/controllers/MercadoPagoController.js
+++ src/controllers/MercadoPagoController.js
@@ -1,20 +1,25 @@
 'use strict'
 
-function renderPayload (body) {
-  const { id, img, title, price, unit } = body
-  return { id, img, title, price, unit }
-}
+const mercadopago = require('mercadopago')
+const { isValidObject } = require('../utils/helpers')
+const { mercadoPagoInit, renderPayload } = require('../utils/mercadoPago')
 
 class MercadoPagoController {
-  store (req, res) {
+  async store (req, res) {
     try {
-      return res.json(renderPayload(req.body))
-    } catch (error) {
-      const { status: code, message } = error
-      return res.status(status).json({
-        code,
-        message
+      await mercadoPagoInit()
+
+      const payload = renderPayload(req.body)
+      const response = await mercadopago.preferences.create(payload)
+      const data = response && isValidObject(response.body) ? response.body : {}
+
+      return res.json({
+        init_point: response.body.init_point,
+        sandbox_init_point: response.body.sandbox_init_point,
+        body: data
       })
+    } catch (error) {
+      return res.json(error)
     }
   }
 
---
+++ src/utils/mercadoPago.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const mercadopago = require('mercadopago')
+
+module.exports = {
+  async mercadoPagoInit () {
+    await mercadopago.configure({
+      sandbox: true,
+      access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN,
+      'x-integrator-id': process.env.MERCADO_PAGO_INTEGRATOR_ID
+    })
+  },
+
+  renderPayload (body) {
+    const { id, img, title, price, unit } = body
+
+    return {
+      collector_id: Number(process.env.MERCADO_PAGO_COLLECTOR_ID),
+      items: [
+        {
+          id,
+          title,
+          currency_id: 'BRL',
+          picture_url: img,
+          description: title,
+          category_id: 'mobile',
+          quantity: unit,
+          unit_price: price
+        }
+      ],
+      payer: {
+        name: 'Lalo',
+        surname: 'Landa',
+        email: 'test_user_92801501@testuser.com',
+        phone: {
+          area_code: '55',
+          number: 985298743
+        },
+        identification: {
+          type: 'CPF',
+          number: '27482519033'
+        },
+        address: {
+          street_name: 'Insurgentes Sur',
+          street_number: 1602,
+          zip_code: '78134-190'
+        }
+      },
+      payment_methods: {
+        excluded_payment_methods: [
+          {
+            id: 'amex'
+          }
+        ],
+        installments: 6
+      },
+      back_urls: {
+        success: 'https://ederssouza-mp-ecommerce-nodejs.herokuapp.com/checkout/pagamento-aprovado',
+        pending: 'https://ederssouza-mp-ecommerce-nodejs.herokuapp.com/checkout/pagamento-pendente',
+        failure: 'https://ederssouza-mp-ecommerce-nodejs.herokuapp.com/checkout/pagamento-rejeitado'
+      },
+      auto_return: 'approved',
+      notification_url: 'https://ederssouza-mp-ecommerce-nodejs.herokuapp.com/api/v1/mercadopago/notifications',
+      // notification_url: 'https://webhook.site/3710f3f0-5853-4892-a828-cb548cb05b1f',
+      statement_descriptor: 'Tienda e-commerce',
+      external_reference: 'teste@outlook.com.br' // TO DO
+    }
+  }
+}
